d41c34e118c117d1cb04fcebff8892758c0762b5
hex0xyz <70755374+Hades0385@users.noreply.github.com>
2023-11-10 23:08:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ $(document).ready(function() {
 $('#google-form').submit(function (e) {
         e.preventDefault(); // 阻止表單的預設提交行為
     
-        // 獲取表單資料
+        
         var formData = {
-        'entry.1145403706': $('#send').val() // 這裡的 entry.1234567890 要替換為你 Google 表單中對應的輸入欄位的 ID
+        'entry.1145403706': $('#send').val() 
         };
     
         // Google 表單的網址
-        var googleFormURL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeyjVLSKfxMMmWldNmSUaA2JaFwWCnZ39lWimTzwYw60aVA2A/formResponse'; // 請將 your_form_id 替換為你 Google 表單的 ID
+        var googleFormURL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeyjVLSKfxMMmWldNmSUaA2JaFwWCnZ39lWimTzwYw60aVA2A/formResponse'; 
     
         // 使用 AJAX 發送資料到 Google 表單
         $.ajax({
@@ -39,13 +39,10 @@ $('#google-form').submit(function (e) {
             $('#google-form').removeClass('is-invalid');
         },
         success: function(response) {
-            // 成功處理後的動作
             console.log('成功提交表單');
         },
         error: function(error) {
-            // 錯誤處理
             console.error('提交表單失敗');
-            // 這裡可以加入錯誤發生時的處理
         }
         });
     
